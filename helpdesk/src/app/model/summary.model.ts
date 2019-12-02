2c6568b84b8228c9856b46f4e9123d0667108212
Luis Gabriel <luisgabrielbento@hotmail.com>
2019-12-02 02:47:12 -0300

    Finalizando o front end

---
+++ helpdesk/src/app/model/summary.model.ts
@@ -0,0 +1,8 @@
+export class SummaryModel {
+    public amountNew: number;
+	public amountResolved: number;
+	public amountApproved: number;
+	public amountDisapproved: number;
+	public amountAssigned: number;
+	public amountClosed: number;        
+}
